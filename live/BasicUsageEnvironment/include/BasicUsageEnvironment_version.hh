03922d90289fd0a648b84085d064db19ac1ffeea
Ralf Globisch <rglobisch@csir.co.za>
2017-04-20 11:05:38 +0200

    LiveMedia version 2017.04.10

---
+++ live/BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "BasicUsageEnvironment" library
+// Copyright (c) 1996-2017 Live Networks, Inc.  All rights reserved.
+
+#ifndef _BASICUSAGEENVIRONMENT_VERSION_HH
+#define _BASICUSAGEENVIRONMENT_VERSION_HH
+
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2017.04.10"
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1491782400
+
+#endif
