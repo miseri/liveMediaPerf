5c68d187eb5efb95cf5af65cbede505aec5495b1
Ralf Globisch <ralf.globisch@gmail.com>
2017-04-21 11:28:06 +0200

    Script to download test .264 files from live555.com

---
+++ getTestMediaContentFromLive555.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget http://www.live555.com/liveMedia/public/264/slamtv10.264 -O live/mediaServer/slamtv10.264
+wget http://www.live555.com/liveMedia/public/264/slamtv60.264 -O live/mediaServer/slamtv60.264
+wget http://www.live555.com/liveMedia/public/264/tc10.264 -O live/mediaServer/tc10.264
+wget http://www.live555.com/liveMedia/public/264/test.264 -O live/mediaServer/test.264
+
