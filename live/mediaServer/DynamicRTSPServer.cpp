c36712c4307c8d9304aa579803d0ea7d878b8d37
Ralf Globisch <ralf.globisch@gmail.com>
2017-07-17 20:59:54 +0200

    Added aac audio session to measure video and audio performance of RTSP streaming

--- live/mediaServer/DynamicRTSPServer.cpp
+++ live/mediaServer/DynamicRTSPServer.cpp
@@ -138,8 +138,10 @@ static ServerMediaSession* createNewSMS(UsageEnvironment& env,
   } else if (strcmp(extension, ".264") == 0) {
     // Assumed to be a H.264 Video Elementary Stream file:
     NEW_SMS("H.264 Video");
-    OutPacketBuffer::maxSize = 100000; // allow for some possibly large H.264 frames
+    OutPacketBuffer::maxSize = 300000; // allow for some possibly large H.264 frames
     sms->addSubsession(H264VideoFileServerMediaSubsession::createNew(env, fileName, reuseSource));
+    // HACK RG: to test audio and video subsessions
+    sms->addSubsession(ADTSAudioFileServerMediaSubsession::createNew(env, "test.aac", reuseSource));
   } else if (strcmp(extension, ".265") == 0) {
     // Assumed to be a H.265 Video Elementary Stream file:
     NEW_SMS("H.265 Video");
