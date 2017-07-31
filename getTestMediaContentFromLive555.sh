e0e56462af71e3ef7d75694e6c4c78ec526d2f24
Ralf Globisch <ralf.globisch@gmail.com>
2017-07-31 12:45:59 +0200

    Added download of test.aac

--- getTestMediaContentFromLive555.sh
+++ getTestMediaContentFromLive555.sh
@@ -4,4 +4,6 @@ wget http://www.live555.com/liveMedia/public/264/slamtv10.264 -O live/mediaServe
 wget http://www.live555.com/liveMedia/public/264/slamtv60.264 -O live/mediaServer/slamtv60.264
 wget http://www.live555.com/liveMedia/public/264/tc10.264 -O live/mediaServer/tc10.264
 wget http://www.live555.com/liveMedia/public/264/test.264 -O live/mediaServer/test.264
+wget http://www.live555.com/liveMedia/public/aac/test.aac -O live/mediaServer/test.aac
+
 
